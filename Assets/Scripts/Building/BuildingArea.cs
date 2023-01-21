c452c24d39bde20302319508a911d3e09842d34e
frog016 <mixalyov.nikita@mail.ru>
2023-01-21 18:04:46 +0500

    Building
    Made centering on the grid.

--- Assets/Scripts/Building/BuildingArea.cs
+++ Assets/Scripts/Building/BuildingArea.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 using Zenject;
 
 public class BuildingArea : MonoBehaviour
@@ -7,11 +8,13 @@ public class BuildingArea : MonoBehaviour
     [SerializeField] private LayerMask _layer;
 
     private IFactory _factory;
+    private Tilemap _tilemap;
 
     [Inject]
-    public void Constructor(IFactory factory)
+    public void Constructor(IFactory factory, Tilemap tilemap)
     {
         _factory = factory;
+        _tilemap = tilemap;
     }
 
     public void Build(Building building, Vector3 position, Quaternion rotation)
@@ -19,11 +22,19 @@ public class BuildingArea : MonoBehaviour
         if (!IsEmpty(position))
             throw new InvalidOperationException("The position you have chosen is occupied. Please check it before placing.");
 
-        var createdBuilding = _factory.CreateFromPrefabForComponent(building, position, rotation);
+        var clampedPosition = GetCellCenterPosition(position);
+        var createdBuilding = _factory.CreateFromPrefabForComponent(building, clampedPosition, rotation);
     }
 
     public bool IsEmpty(Vector3 position)
     {
         return !Physics.CheckBox(position, Vector3.one / 2, Quaternion.identity, _layer);
+    }
+
+    private Vector3 GetCellCenterPosition(Vector3 position)
+    {
+        var clampedPosition = _tilemap.GetCellCenterWorld(_tilemap.WorldToCell(position));
+        clampedPosition.y = position.y;
+        return clampedPosition;
     }
 }
